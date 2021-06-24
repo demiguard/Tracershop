575c7666e6ad1b187d990898e9de22cc87abf7de
demiguard <Christoffer.Vilstrup.Jensen@regionh.dk>
2021-06-24 14:39:26 +0200

    Forgot this in the last commit

--- tracershop/customer/modelsDir/serverModels.py
+++ tracershop/customer/modelsDir/serverModels.py
@@ -7,3 +7,4 @@ class ServerConfiguration(SubscribeableModel):
   ID = models.AutoField(primary_key=True)
   ExternalDatabase = models.ForeignKey(Database, on_delete=models.SET_NULL, blank=True, null=True)
   SMTPServer = models.GenericIPAddressField(default="10.140.209.2")
+  DefaultCalculatorValue = models.IntegerField(default=300)
