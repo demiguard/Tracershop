975a3dded376fbab89a4082234e9856c17a27528
demiguard <Christoffer.Vilstrup.Jensen@regionh.dk>
2021-07-01 16:00:58 +0200

    started production

--- production/production/settings.py
+++ production/production/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pcsy3wswz(mmips=koz#kj_q=1-z5val90mn$o-o-e@&lc6+6!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'frontend.apps.FrontendConfig'
+    'frontend.apps.FrontendConfigx½'
 ]
 
 MIDDLEWARE = [
