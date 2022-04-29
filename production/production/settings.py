34d9e437a8b7503e3694b534bad3a8a87d11dc75
demiguard <Christoffer.Vilstrup.Jensen@regionh.dk>
2022-04-29 14:55:48 +0200

    wRONG FOLDER

--- production/production/settings.py
+++ production/production/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from production.SECRET_KEY import KEY
-from config import debug_file_log
+from production.config import debug_file_log
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
