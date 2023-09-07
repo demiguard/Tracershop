b4321401a8a7651ec266fc9df88e9f418555bd63
demiguard <demiguard@github.com>
2023-09-07 14:58:50 +0200

    Made Isotope review

--- production/frontend/src/components/production_pages/setup_pages/isotope_setup_page.js
+++ production/frontend/src/components/production_pages/setup_pages/isotope_setup_page.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
-import { Col, Container, Row, Table } from 'react-bootstrap';
-import { JSON_ISOTOPE, PROP_WEBSOCKET } from '../../../lib/constants';
+import { Col, Container, FormCheck, FormControl, InputGroup, Row, Table } from 'react-bootstrap';
+import { JSON_ISOTOPE, PROP_WEBSOCKET, cssCenter } from '../../../lib/constants';
+import { TracershopInputGroup } from '../../injectable/tracershop_input_group';
+import { setStateToEvent } from '../../../lib/state_management';
+import { ClickableIcon } from '../../injectable/icons';
 
 function NewIsotopeRow({websocket}){
   return (<tr>
@@ -14,14 +17,74 @@ function NewIsotopeRow({websocket}){
 }
 
 function IsotopeRow({isotope ,websocket}){
-  return <tr>
-    <td>{isotope.atomic_number}</td>
-    <td>{isotope.atomic_mass}</td>
-    <td>{isotope.halflife_seconds}</td>
-    <td>{isotope.atomic_letter}</td>
-    <td>{isotope.metastable}</td>
-    <td></td>
-  </tr>
+
+  const [atomicLetter, setAtomicLetter] = useState(isotope.atomic_letter);
+  const [atomicNumber, setAtomicNumber] = useState(isotope.atomic_number);
+  const [atomicMass, setAtomicMass] = useState(isotope.atomic_mass);
+  const [halflifeSeconds, setHalflifeSeconds] = useState(isotope.halflife_seconds);
+  const [metastable, setMetastable] = useState(isotope.metastable);
+
+  const changed = !(atomicLetter === isotope.atomic_letter
+               && Number(atomicNumber) === isotope.atomic_number
+               && Number(atomicMass) === isotope.atomic_mass
+               && Number(halflifeSeconds) === isotope.halflife_seconds
+               && metastable === isotope.metastable)
+
+  console.log(atomicLetter === isotope.atomic_letter,
+    Number(atomicNumber) === isotope.atomic_number,
+    Number(atomicMass) === isotope.atomic_mass,
+    Number(halflifeSeconds) === isotope.halflife_seconds,
+    metastable === isotope.metastable)
+
+  function updateIsotope(){
+
+  }
+
+  return (<tr>
+    <td>
+      <TracershopInputGroup label="Bogstav">
+        <FormControl 
+          value={atomicLetter}
+          onChange={setStateToEvent(setAtomicLetter)}
+        />
+      </TracershopInputGroup>
+    </td>
+    <td>
+      <FormControl
+        value={atomicNumber}
+        onChange={setStateToEvent(setAtomicNumber)}
+      />
+    </td>
+    <td>
+      <FormControl
+        value={atomicMass}
+        onChange={setStateToEvent(setAtomicMass)}
+      />
+    </td>
+    <td>
+      <TracershopInputGroup label="Halveringstid">
+        <FormControl
+          value={halflifeSeconds}
+          onChange={setStateToEvent(setHalflifeSeconds)}
+        />
+        <InputGroup.Text>
+          Sekunder
+        </InputGroup.Text>
+      </TracershopInputGroup>
+    </td>
+    <td>
+      <FormCheck
+        onChange={() => {setMetastable(!metastable)}}
+        checked={metastable}
+        />
+    </td>
+    <td>
+      {changed ? <ClickableIcon
+        src="/static/images/update.svg"
+        onclick={updateIsotope}
+      /> : ""}
+    </td>
+  </tr>)
 }
 
 
