0fb853ebd8a2acaf24bdaa933cdd07f3da82a823
Christoffer Vilstrup Jensen <demiguard@gmail.com>
2024-05-27 12:51:40 +0200

    Fixes to bash scripts

--- production/database/management/bash_scripts/export_orders.sh
+++ production/database/management/bash_scripts/export_orders.sh
@@ -5,7 +5,7 @@ if [[ $# -ne 1 ]]; then
   exit 1
 fi
 
-query="'select * from orders where deliver_datetime > '$1' and deliver_datetime < '2030-01-01''"
+query="'select \* from orders where deliver_datetime > $1 and deliver_datetime < 2030-01-01'"
 #echo $query
 
 mysql -D TracerShop -h 127.0.0.1 -P 5000 -B -e $query | tr '\t' ',' > vials.csv
--- production/database/management/bash_scripts/export_vials.sh
+++ production/database/management/bash_scripts/export_vials.sh
@@ -5,7 +5,7 @@ if [[ $# -ne 1 ]]; then
   exit 1
 fi
 
-query="'select * from VAL where filldate > '$1' and filldate < '2030-01-01''"
+query="'select \* from VAL where filldate > $1 and filldate < 2030-01-01'"
 #echo $query
 
 mysql -D TracerShop -h 127.0.0.1 -P 5000 -B -e $query | tr '\t' ',' > vials.csv
