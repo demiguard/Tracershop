5177af73f7ce4b81f774dfe57d0e693c10297638
Christoffer Vilstrup Jensen <demiguard@gmail.com>
2024-07-03 09:49:37 +0200

    bugfix

--- production/frontend/src/components/production_pages/activity_table.js
+++ production/frontend/src/components/production_pages/activity_table.js
@@ -400,7 +400,7 @@ export function ActivityTable ({active_tracer, active_date}) {
   }
 
   let available_days_html = <div></div>;
-  if(productionRows.length === 0){
+  if(productionRows.length === 0 && active_tracer != -1){
     const /**@type {ActivityProduction[]} */ weekly_productions = productionsFilter(state, {tracerID : active_tracer});
     const /**@type {Set<Number>} */ production_days = new Set();
     for(const production of weekly_productions){
