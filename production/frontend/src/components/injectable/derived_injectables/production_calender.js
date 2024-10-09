772090c485774412fd1cae39fa439db0a42ac454
demiguard <demiguard@gmail.com>
2024-10-09 08:54:33 +0200

    Jacob didn't like the feature :(

--- production/frontend/src/components/injectable/derived_injectables/production_calender.js
+++ production/frontend/src/components/injectable/derived_injectables/production_calender.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { Calender } from '../calender';
 import { useTracershopState} from '~/components/tracer_shop_context';
 import { ProductionBitChain } from '~/lib/data_structures';
-import { toMapping } from '~/lib/utils';
-import { productionsFilter, timeSlotsFilter } from '~/lib/filters';
-
 
 /**
  *
@@ -16,26 +13,12 @@ import { productionsFilter, timeSlotsFilter } from '~/lib/filters';
  */
 export function ProductionCalender({active_date, on_day_click, activeTracer}){
   const state = useTracershopState();
-
-  const productions = 0 < activeTracer ? toMapping(productionsFilter(state, {tracerID : activeTracer})) :
-    state.production;
-
-  const bitChain = new ProductionBitChain(productions);
-
-  const filter_activity_function = (() => {
-    if(activeTracer <= 0){
-      return () => true;
-    }
-    const timeSlots = timeSlotsFilter(
-      state, {state : state, tracerID : activeTracer}, true
-    );
-    return (activityOrder) => timeSlots.includes(activityOrder.ordered_time_slot);
-  })()
+  const bitChain = new ProductionBitChain(state.production);
 
   return <Calender
     calender_date={active_date}
     calender_on_day_click={on_day_click}
-    filter_activity_orders={filter_activity_function}
+    filter_activity_orders={() => true}
     filter_injection_orders={() => true}
     bit_chain={bitChain}
   />;
