a3dfd9d350ada144bcb88c4813959540e0d3e44e
Mathias Bardram Johnbeck <Mathiasjohnbeck@gmail.com>
2024-11-14 15:10:14 +0100

    More indepth testing of moniter_home_page

--- production/frontend/src/tests/components/production_pages/monitor_pages/monitor_home_page.test.js
+++ production/frontend/src/tests/components/production_pages/monitor_pages/monitor_home_page.test.js
@@ -10,8 +10,9 @@ import { ActivityTable } from "~/components/production_pages/activity_table.js"
 import { ORDER_STATUS, PROP_ACTIVE_DATE, PROP_ACTIVE_TRACER } from "~/lib/constants.js";
 import { testState } from "~/tests/app_state.js";
 import { StateContextProvider, WebsocketContextProvider } from "~/components/tracer_shop_context.js";
-import { InjectionOrder } from "~/dataclasses/dataclasses";
-import { separatorInjectionOrders } from "~/components/production_pages/monitoring_pages/monitor_home_page";
+import { InjectionOrder, ActivityOrder, TracershopState } from "~/dataclasses/dataclasses";
+import { separatorInjectionOrders, separatorActivityOrders } from "~/components/production_pages/monitoring_pages/monitor_home_page";
+import { object } from "prop-types";
 
 
 const module = jest.mock('~/lib/tracer_websocket.js');
@@ -40,7 +41,33 @@ afterEach(() => {
   props=null
 });
 
-describe("Separators test suite", () => {
+describe("InjectionOrder Separators test suite", () => {
+  
+  it("not released", () => {
+    const testData = [
+      new InjectionOrder(1,
+                         "12:30:00",
+                         "2000/05/05",
+                         1,
+                         ORDER_STATUS.ACCEPTED,
+                         0,
+                         null,
+                         4,
+                         1,
+                         1,
+                         "BlaBla",
+                         null, 2)
+    ];
+
+    const res = separatorInjectionOrders(testData);
+
+    expect(res.total).toEqual(1);
+    expect(res.releasedOnTime).toEqual(0);
+    expect(res.notReleased).toEqual(1);
+  })
+  
+  
+  
   it("On time", () => {
     const testData = [
       new InjectionOrder(1,
@@ -112,4 +139,109 @@ describe("Separators test suite", () => {
     expect(res.releasedDelayed30).toEqual(0);
     expect(res.releasedDelayed30Plus).toEqual(1);
   });
+
 });
+
+describe("ActivityOrders Separators test suite", () => {
+
+  const state = new TracershopState();
+  Object.assign(state, testState);
+
+  it("not released", () => {
+    const testData = [
+      new ActivityOrder(1,
+                        12345,
+                        "2010/05/05",
+                        ORDER_STATUS.ACCEPTED,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null
+      )
+    ];
+
+    const res = separatorActivityOrders(testData, state);
+
+    expect(res.total).toEqual(1);
+    expect(res.releasedOnTime).toEqual(0);
+    expect(res.notReleased).toEqual(1);
+  })
+  
+  
+  
+  it("On time", () => {
+    const testData = [
+      new ActivityOrder(1,
+                        12345,
+                        "2016/05/05",
+                        ORDER_STATUS.RELEASED,
+                        null,
+                        1, //08:15:00
+                        null,
+                        "2016/05/05 08:15:00",
+                        null,
+                        null
+      )
+    ];
+
+    const res = separatorActivityOrders(testData, state);
+
+    expect(res.total).toEqual(1);
+    expect(res.releasedOnTime).toEqual(1);
+    expect(res.releasedDelayed30).toEqual(0);
+    expect(res.releasedDelayed30Plus).toEqual(0);
+  });
+
+  
+
+  it("Delayed 15 min", () => {
+    const testData = [
+      new ActivityOrder(1,
+                        12345,
+                        "2016/05/05",
+                        ORDER_STATUS.RELEASED,
+                        null,
+                        1, //08:15:00
+                        3, //moved to 09:15:00
+                        "2016/05/05 09:30:00",
+                        null,
+                        null
+      )
+    ];
+
+    const res = separatorActivityOrders(testData, state);
+
+    expect(res.total).toEqual(1);
+    expect(res.releasedOnTime).toEqual(0);
+    expect(res.releasedDelayed30).toEqual(1);
+    expect(res.releasedDelayed30Plus).toEqual(0);
+  });
+
+  it("Delayed 60 min", () => {
+    const testData = [
+      new ActivityOrder(1,
+                        12345,
+                        "2016/05/05",
+                        ORDER_STATUS.RELEASED,
+                        null,
+                        1, //08:15:00
+                        null,
+                        "2016/05/05 09:15:00",
+                        null,
+                        null
+      )    
+    ];
+
+    const res = separatorActivityOrders(testData, state);
+
+    expect(res.total).toEqual(1);
+    expect(res.releasedOnTime).toEqual(0);
+    expect(res.releasedDelayed30).toEqual(0);
+    expect(res.releasedDelayed30Plus).toEqual(1);
+  });
+
+})
+
+
