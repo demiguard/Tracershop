18fb2732a70b4720445ecedaf93fe3b1aaa619be
Christoffer Vilstrup Jensen <demiguard@gmail.com>
2026-08-31 12:59:25 +0200

    Upgraded to javascript 2.6

--- production/frontend/src/lib/shared_constants.js
+++ production/frontend/src/lib/shared_constants.js
@@ -83,7 +83,7 @@ export const EXCLUDED_STATE_MODELS = [
   "telemetry_request",
 ]
 
-export const JAVASCRIPT_VERSION = "2.5.0";
+export const JAVASCRIPT_VERSION = "2.6.0";
 
 export const MessageValidationResult = {
   Successful : 0,
